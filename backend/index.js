f8a3fc20b98ce282003b24617f8daf3360c42ef6
Raghav Goel <raghavgoel1453@gmail.com>
2024-01-23 21:55:27 +0530

    feat: added dotenv to backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+require('dotenv').config()
 
-const PORT = 3000;
+const PORT = 3000
 app.listen(PORT, () => {
   console.log(`The server is up and is running on the port: ${PORT}`)
 })
